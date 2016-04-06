61b80a0519b414527cce359b3d10e1046eaafe41
Josh Larson <jplhomer@gmail.com>
2016-04-05 22:03:46 -0500

    Create Meals table

---
+++ database/migrations/2016_04_06_025701_create_meals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index();
+            $table->string('name');
+            $table->date('day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('meals');
+    }
+}
