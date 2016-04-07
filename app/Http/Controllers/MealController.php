ed41ce80a949043168981c6cb3ee492173e81626
Josh Larson <jplhomer@gmail.com>
2016-04-06 20:40:45 -0500

    Add meal adding logic

--- app/Http/Controllers/MealController.php
+++ app/Http/Controllers/MealController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Meal;
+
 use App\Http\Requests;
 
 class MealController extends Controller
@@ -11,7 +13,26 @@ class MealController extends Controller
     public function __construct() {
         return $this->middleware('auth');
     }
+
     public function index(Request $request) {
-        return view('meals.index');
+        $meals = Meal::where('user_id', $request->user()->id)->get();
+
+        return view('meals.index', [
+            'meals' => $meals,
+        ]);
+    }
+
+    public function store(Request $request) {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'day' => 'required|max:255'
+        ]);
+
+        $request->user()->meals()->create([
+            'name' => $request->name,
+            'day' => $request->day,
+        ]);
+
+        return redirect('/meals');
     }
 }
--- resources/views/meals/index.blade.php
+++ resources/views/meals/index.blade.php
@@ -8,7 +8,39 @@
                 <div class="panel-heading">Meals</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <!-- Display Validation Errors -->
+                    @include('common.errors')
+
+                    <form class="form-horizontal" action="{{ url('meal') }}" method="post">
+                        {!! csrf_field() !!}
+
+                        <!-- Meal Name -->
+                        <div class="form-group">
+                            <label for="meal-name" class="col-sm-3 control-label">Meal</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="name" id="meal-name" class="form-control">
+                            </div>
+                        </div>
+
+                        <!-- Meal Day -->
+                        <div class="form-group">
+                            <label for="meal-day" class="col-sm-3 control-label">Day</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="day" id="meal-day" class="form-control">
+                            </div>
+                        </div>
+
+                        <!-- Add Task Button -->
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fa fa-plus"></i> Add Meal
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
